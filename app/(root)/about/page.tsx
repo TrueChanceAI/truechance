10e337d9a60dd8a7ee843438beffb6da85975522
Shuja-ud-din <shuja1339@gmail.com>
2025-09-05 07:28:30 +0500

    chore: email updated on about page

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -334,6 +334,7 @@ export default function AboutPage() {
                       d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
+
                   <span className="text-zinc-300 text-lg">
                     truechanceksa@gmail.com
                   </span>
